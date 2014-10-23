452f033d65036a17a9879265413897f15bc6b9b0
savaki <matt.ho@gmail.com>
2014-10-23 00:33:55 -0700

    - bubbles with time manipulation

--- src-sub/main/core.cljs
+++ src-sub/main/core.cljs
@@ -5,13 +5,45 @@
 
 (def color (atom "#ffffff"))
 
+(def bubbles-state (atom []))
+
+(def step (atom -1))
+
+(defn reverse-step []
+  (if (= @step 1)
+    (reset! step -1)
+    (reset! step 1)))
+
+(defn bubble-move [bubble]
+  {:color (get bubble :color)
+   :pos (+ (get bubble :pos) @step)})
+
 (defn receive-color [new-color]
+  (swap! bubbles-state #(cons {:color new-color :pos 90} %))
+  (println "bubbles =>" @bubbles-state)
   (reset! color new-color))
 
+(defn bubble-view [bubble]
+  (let [color (get bubble :color)
+        pos (get bubble :pos)]
+    (if (and (> pos 0) (<= pos 90))
+      [:div.bubble {:style {:margin-left (str pos "%")
+                            :background-color color}}])))
+
+(defn bubbles-view []
+  [:div.bubbles (for [bubble @bubbles-state]
+                  [bubble-view bubble])])
+
 (defn app-view []
-  [:div.main {:style {:background-color @color}} [:div.container "hello world"]])
+  [:div.main {:style {:background-color @color}} [:div.container [bubbles-view]]
+   [:div.container [:div#send-color {:on-click #(reverse-step)} "Reverse Time"]]])
+
+(defn tick []
+  (swap! bubbles-state (fn [x] (map bubble-move x)))
+  (js/setTimeout #(tick) 40))
 
 (defn app-boot []
+  (tick)
   (println "booting application")
   (let [publish-key "pub-c-0929351a-e2cf-4d06-8f41-2e4b682e7490"
         subscribe-key "sub-c-493cca24-59fb-11e4-a91d-02ee2ddab7fe"]
